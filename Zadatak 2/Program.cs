f26ae736907eb984d5c924b7a59e7509fa4e1ae8
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-06-06 13:40:13 +0200

    Add files via upload

---
+++ Zadatak 2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int UneseneSekunde, minute, sekunde;
+
+            Console.WriteLine("Unesi broj sekundi:");
+
+             UneseneSekunde = Convert.ToInt32(Console.ReadLine());
+
+            minute = UneseneSekunde / 60;
+
+            sekunde = UneseneSekunde % 60;
+
+            Console.WriteLine("Broj minuta =" + minute);
+
+
+
+            Console.WriteLine("Broj sekunda =" + sekunde);
+
+            Console.ReadKey();
+            
+            
+            
+        }
+    }
+}
